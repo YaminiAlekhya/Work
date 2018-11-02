ac4cf2f6703dd2630159a01203b7c8a685613f7e
Yamini <durgaalekhya@gmail.com>
2018-11-02 18:07:28 +0530

    displays calender

---
+++ calender.java
@@ -0,0 +1,10 @@
+import java.util.date;
+
+public class calender
+{
+	public statis void main(string arg[])
+	{
+		jave.util.calender c= java.util.calender.getInstance();
+		System.out.println(c.getTime());
+	}
+} 
