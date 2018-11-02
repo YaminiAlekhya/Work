f4f6a1866630b18e90bc2aa97910f7c8acaa9d81
Yamini <durgaalekhya@gmail.com>
2018-11-02 15:15:31 +0530

    printing the values of a and b:

---
+++ main.java
@@ -0,0 +1,16 @@
+pubic class main
+{
+	private int a=0;
+	final int b=1;
+
+	void generateList()
+	{
+		while(a=20)
+		{
+			a++;
+			b=a;
+			System.out.println("the value of a is:"+ " "+a);
+			System.out.println("the value of b is:"+ " "+b);
+		}
+	}
+}
